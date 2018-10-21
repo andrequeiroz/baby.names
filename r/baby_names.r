32640f5a1ab276fd68e83666dd7dcbe4f433536a
André Queiroz <chegadetentar@gmail.com>
2018-10-21 02:09:35 -0300

    RPostgres and code formatting on r script

--- r/baby_names.r
+++ r/baby_names.r
@@ -1,68 +1,71 @@
-library(RPostgreSQL)
+library(DBI)
 library(dplyr)
 library(ggplot2)
 
-link <- dbConnect(dbDriver("PostgreSQL"), dbname = "baby_names")
+link <- dbConnect(RPostgres::Postgres(), dbname = "baby_names")
 
 abs_freq_map <- function(name, sex) {
-    name <- deparse(substitute(name))
-    sex <- deparse(substitute(sex))
 
-    paste0("SELECT lower(s.st_abbr) AS state, sum(r.total) AS total
-            FROM registries AS r
-             INNER JOIN names AS n ON r.nm_code = n.nm_code
-             INNER JOIN states AS s ON r.st_abbr = s.st_abbr
-            WHERE r.st_abbr <> 'DC'
-             AND n.nm_label = '", name, "' AND r.sex_code = '", sex, "'
-            GROUP BY s.st_abbr") %>%
-        dbGetQuery(link, .) %>%
-        right_join(map_data("state"), by = c("state" = "region")) %>%
-        ggplot() +
-        geom_polygon(mapping = aes(x = long, y = lat,
-                                   group = group, fill = total),
-                     colour = "black", size = 0.2) +
-        scale_fill_gradient(low = "red", high = "green", na.value = NA,
-                            labels = function(x) format(x, big.mark = ".",
-                                                        decimal.mark = ",",
-                                                        scientific = FALSE)) +
-        coord_fixed(1.7) +
-        theme(title = element_blank(), axis.text = element_blank(),
-              axis.ticks = element_blank())
+  name <- deparse(substitute(name))
+  sex <- deparse(substitute(sex))
+
+  paste0("SELECT lower(s.st_name) AS state, sum(r.total) AS total
+          FROM registries AS r
+           INNER JOIN names AS n ON r.nm_code = n.nm_code
+           INNER JOIN states AS s ON r.st_abbr = s.st_abbr
+          WHERE r.st_abbr <> 'DC'
+           AND n.nm_label = '", name, "' AND r.sex_code = '", sex, "'
+          GROUP BY s.st_abbr") %>%
+    dbGetQuery(link, .) %>%
+    mutate(total = as.integer(total)) %>%
+    right_join(map_data("state"), by = c("state" = "region")) %>%
+    ggplot() +
+    geom_polygon(mapping = aes(x = long, y = lat,
+                               group = group, fill = total),
+                 colour = "black", size = 0.2) +
+    scale_fill_gradient(low = "red", high = "green", na.value = NA,
+                        labels = function(x) format(x, big.mark = ".",
+                                                    decimal.mark = ",",
+                                                    scientific = FALSE)) +
+    coord_fixed(1.7) +
+    theme(title = element_blank(), axis.text = element_blank(),
+          axis.ticks = element_blank())
 }
 
 rel_freq_map <- function(name, sex) {
-    name <- deparse(substitute(name))
-    sex <- deparse(substitute(sex))
 
-    paste0("SELECT res.state_f AS state, res.freq::numeric / res.total AS prop
-            FROM ((SELECT lower(s.st_abbr) AS state_f, sum(r.total) AS freq
-                   FROM registries AS r
-                    INNER JOIN names AS n ON r.nm_code = n.nm_code
-                    INNER JOIN state AS s ON r.st_abbr = s.st_abbr
-                   WHERE r.st_abbr <> 'DC'
-                    AND n.nm_label = '", name, "' AND r.sex_code = '", sex, "'
-                   GROUP BY s.st_abbr) AS filter
-                   INNER JOIN (SELECT lower(s.st_abbr) AS state_t,
-                                sum(r.total) AS total
-                               FROM registries AS r
-                                INNER JOIN state AS s ON r.st_abbr = s.st_abbr
-                               WHERE r.st_abbr <> 'DC'
-                                AND r.sex_code ='", sex, "'
-                               GROUP BY s.st_abbr) AS total
-                    ON filter.state_f = total.state_t) AS res") %>%
-        dbGetQuery(link, .) %>%
-        right_join(map_data("state"), by = c("state" = "region")) %>%
-        ggplot() +
-        geom_polygon(mapping = aes(x = long, y = lat,
-                                   group = group, fill = prop),
-                     colour = "black", size = 0.2) +
-        scale_fill_gradient(low = "red", high = "green", na.value = NA,
-                            labels = function(x) format(x, big.mark = ".",
-                                                        decimal.mark = ",",
-                                                        scientific = FALSE)) +
-        coord_fixed(1.7) +
-        theme(title = element_blank(), axis.text = element_blank(),
-              axis.ticks = element_blank())
+  name <- deparse(substitute(name))
+  sex <- deparse(substitute(sex))
+
+  paste0("SELECT res.state_f AS state, res.freq::numeric / res.total AS prop
+          FROM ((SELECT lower(s.st_name) AS state_f, sum(r.total) AS freq
+                 FROM registries AS r
+                  INNER JOIN names AS n ON r.nm_code = n.nm_code
+                  INNER JOIN states AS s ON r.st_abbr = s.st_abbr
+                 WHERE r.st_abbr <> 'DC'
+                  AND n.nm_label = '", name, "' AND r.sex_code = '", sex, "'
+                 GROUP BY s.st_name) AS filter
+                  INNER JOIN (SELECT lower(s.st_name) AS state_t,
+                               sum(r.total) AS total
+                              FROM registries AS r
+                               INNER JOIN states AS s ON r.st_abbr = s.st_abbr
+                              WHERE r.st_abbr <> 'DC'
+                               AND r.sex_code ='", sex, "'
+                              GROUP BY s.st_name) AS total
+                   ON filter.state_f = total.state_t) AS res") %>%
+    dbGetQuery(link, .) %>%
+    right_join(map_data("state"), by = c("state" = "region")) %>%
+    ggplot() +
+    geom_polygon(mapping = aes(x = long, y = lat,
+                               group = group, fill = prop),
+                 colour = "black", size = 0.2) +
+    scale_fill_gradient(low = "red", high = "green", na.value = NA,
+                        labels = function(x) format(x, big.mark = ".",
+                                                    decimal.mark = ",",
+                                                    scientific = FALSE)) +
+    coord_fixed(1.7) +
+    theme(title = element_blank(), axis.text = element_blank(),
+          axis.ticks = element_blank())
 }
 
 abs_freq_map(Alexa, F)
@@ -71,12 +74,12 @@ rel_freq_map(Alexa, F)
 "SELECT s.st_abbr, n.nm_label, sum(r.total) AS total
  FROM registries AS r
   INNER JOIN names AS n ON r.nm_code = n.nm_code
-  INNER JOIN state AS s ON r.st_abbr = s.st_abbr
+  INNER JOIN states AS s ON r.st_abbr = s.st_abbr
  WHERE r.sex_code = 'F'
  GROUP BY s.st_abbr, n.nm_label
  ORDER BY s.st_abbr, total DESC
  LIMIT 10" %>%
-    dbGetQuery(link, .)
+  dbGetQuery(link, .)
 
 "SELECT r.yob, r.sex_code AS sex, sum(r.total) AS total
  FROM registries AS r
@@ -84,9 +87,10 @@ rel_freq_map(Alexa, F)
  WHERE n.nm_label = 'Alexa'
  GROUP BY r.yob, r.sex_code
  ORDER BY r.yob" %>%
-    dbGetQuery(link, .) %>%
-    ggplot() +
-    geom_line(mapping = aes(x = yob, y = total, colour = sex)) +
-    scale_x_continuous(limits = c(1880, 2016))
+  dbGetQuery(link, .) %>%
+  mutate(total = as.integer(total)) %>%
+  ggplot() +
+  geom_line(mapping = aes(x = yob, y = total, colour = sex)) +
+  scale_x_continuous(limits = c(1880, NA))
 
 dbDisconnect(link)
